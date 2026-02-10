d6750abf3e6baa9843074efe4781089c06890067
kimyeonjun0307 <133335763+kimyeonjun0307@users.noreply.github.com>
2026-02-10 19:59:15 +0900

    [Bronze V] Title: We love kriii, Time: 44 ms, Memory: 5328 KB -BaekjoonHub

---
+++ C#/백준/Bronze/10718. We love kriii/We love kriii.cs
@@ -0,0 +1,15 @@
+using System.Text;
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("강한친구 대한육군");
+            Console.WriteLine("강한친구 대한육군");
+        }
+    }
+}
+
+
