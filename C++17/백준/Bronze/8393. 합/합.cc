550ced48f6f1c08bfd579dcb6b4cbb25a001e482
kimyeonjun0307 <133335763+kimyeonjun0307@users.noreply.github.com>
2026-02-12 12:32:18 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/8393. 합/합.cc
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int A;
+    cin>>A;
+    int sum= 0;
+    for (int i = 1; i <= A;i++)
+    {
+        sum+=i;
+    }
+    cout<<sum<<endl;
+    return 0;
+}
