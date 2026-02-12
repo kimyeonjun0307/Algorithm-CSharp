b35186d67a31eab17f20c22ce726669d1bdfff2d
kimyeonjun0307 <133335763+kimyeonjun0307@users.noreply.github.com>
2026-02-12 12:27:58 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int A;
+    cin>>A;
+    for (int i = 0; i < A;i++)
+    {
+        int B,C;
+        cin>>B>>C;
+        cout<<B+C<<endl;
+    }
+    return 0;
+}
