7ac6fee5385495470b4736ffc0f54cba04df1ff0
kimyeonjun0307 <133335763+kimyeonjun0307@users.noreply.github.com>
2026-02-13 16:30:23 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+ {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int A;
+    cin>>A;
+    for(int i=0;i<A;i++)
+    {
+        for(int j= 0; j<i+1; j++)
+        {
+        cout<<"*";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
