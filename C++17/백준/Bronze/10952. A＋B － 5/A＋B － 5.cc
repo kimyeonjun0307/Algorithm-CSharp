5ccb81399ab8649599f61cf38e6916fb48bae974
kimyeonjun0307 <133335763+kimyeonjun0307@users.noreply.github.com>
2026-02-14 04:39:11 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+ {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int A,B;
+    
+    cin>>A>>B;
+    while(A!=0 && B!=0)
+    {
+        
+        cout<<A+B<<"\n";
+        cin>>A>>B;
+    }
+
+    return 0;
+}
