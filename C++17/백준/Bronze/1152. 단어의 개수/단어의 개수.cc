c21e54a2faec39b935a91dcf5ccc1a86fd1ac6dc
kimyeonjun0307 <133335763+kimyeonjun0307@users.noreply.github.com>
2026-02-18 22:58:41 +0900

    [Bronze II] Title: 단어의 개수, Time: 44 ms, Memory: 26728 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string A;
+    vector<string> s;
+    while(cin>>A)
+    {
+        s.push_back(A);
+    }
+    cout<<s.size();
+}
